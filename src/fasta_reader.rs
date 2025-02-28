39db65121b87223485289b93e1f24afebec50fe0
Thomas Svendal <thomassvendal@gmail.com>
2025-02-28 11:49:20 +0100

    Did shortcuts, im now regretting my life

--- src/fasta_reader.rs
+++ src/fasta_reader.rs
@@ -1,4 +1,10 @@
-use std::path::Path;
+use std::{
+    fs::File,
+    io::{BufReader, Read},
+    path::Path,
+};
+
+use itertools::Itertools;
 
 pub enum Base {
     A,
@@ -20,6 +26,36 @@ impl TryFrom<char> for Base {
     }
 }
 
+impl Base {
+    pub fn to_index(&self) -> usize {
+        match self {
+            Base::A => 0,
+            Base::C => 1,
+            Base::G => 2,
+            Base::T => 3,
+        }
+    }
+}
+
 pub fn read_fasta(path: &Path) -> Vec<Vec<Base>> {
-    todo!();
+    let file = File::open(path)
+        .unwrap_or_else(|_| panic!("Could not opne file at path: {}", path.to_str().unwrap()));
+
+    let mut reader = BufReader::new(file);
+    let mut file_contens = String::new();
+    reader.read_to_string(&mut file_contens).unwrap();
+
+    file_contens
+        .split("\n")
+        .chunks(2)
+        .into_iter()
+        .map(|chunk| {
+            chunk
+                .last()
+                .unwrap()
+                .chars()
+                .filter_map(|c| Base::try_from(c.to_ascii_uppercase()).ok())
+                .collect()
+        })
+        .collect()
 }
